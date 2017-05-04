db5ca246218ffac954bcacd595f032886b6482ea
Emil Scoffone <emil@scoffone.com>
2017-05-03 18:14:22 -0700

    Implemented the :update and :destroy actions for the shallow part of the attachments controller

--- app/controllers/fl/framework/attachments_controller.rb
+++ app/controllers/fl/framework/attachments_controller.rb
@@ -2,7 +2,17 @@ require_dependency "fl/framework/application_controller"
 
 module Fl::Framework
   class AttachmentsController < ApplicationController
-    before_action :set_attachment, only: [:show, :edit, :update, :destroy]
+    include Fl::Framework::Controller::Helper
+    include Fl::Framework::Controller::Access
+
+    # This method is a placeholder for access control. Applications that support a notion of a logged-in
+    # users will provide their own implementation, and must remove this one.
+    # For example:
+    # before_action :authenticate_user!, except: [ :index, :show ]
+
+    def current_user()
+      nil
+    end
 
     # GET /attachments
     def index
@@ -11,6 +21,23 @@ module Fl::Framework
 
     # GET /attachments/1
     def show
+      p = normalize_params(params)
+      service = Fl::Framework::Service::Attachment::ActiveRecord.new(Fl::Framework::Comment::ActiveRecord::Comment, current_user, p)
+      if get_and_check(service, Fl::Framework::Access::Grants::READ, '@attachment')
+        respond_to do |format|
+          format.json do
+            render(:json => { :attachment => hash_one_object(@attachment, p[:to_hash]) },
+                   status: :ok)
+          end
+        end
+      else
+        respond_to do |format|
+          format.html
+          format.json do
+            error_response(generate_error_info(service))
+          end
+        end
+      end
     end
 
     # GET /attachments/new
@@ -35,25 +62,55 @@ module Fl::Framework
 
     # PATCH/PUT /attachments/1
     def update
-      if @attachment.update(attachment_params)
-        redirect_to @attachment, notice: 'Attachment was successfully updated.'
-      else
-        render :edit
+      p = normalize_params(params)
+      service = Fl::Framework::Service::Attachment::ActiveRecord.new(Fl::Framework::Comment::ActiveRecord::Comment, current_user, p)
+      if get_and_check(service, Fl::Framework::Access::Grants::WRITE, '@attachment')
+        if @attachment.update_attributes(p[:attachment])
+          respond_to do |format|
+            format.json do
+              render(:json => { :attachment => hash_one_object(@attachment, p[:to_hash]) },
+                     status: :ok)
+            end
+          end
+        else
+          respond_to do |format|
+            format.html
+            format.json do
+              error_response(generate_error_info(service))
+            end
+          end
+        end
       end
     end
 
     # DELETE /attachments/1
     def destroy
-      @attachment.destroy
-      redirect_to attachments_url, notice: 'Attachment was successfully destroyed.'
+      p = normalize_params(params)
+      service = Fl::Framework::Service::Attachment::ActiveRecord.new(Fl::Framework::Comment::ActiveRecord::Comment, current_user, p)
+      if get_and_check(service, Fl::Framework::Access::Grants::DESTROY, '@attachment')
+        fingerprint = @attachment.fingerprint
+        if @attachment.destroy
+          respond_to do |format|
+            format.json do
+              status_response({
+                                status: Fl::Framework::Service::OK,
+                                message: tx('fl.framework.attachment.controller.actions.destroy.deleted',
+                                            fingerprint: fingerprint)
+                              })
+            end
+          end
+        else
+          respond_to do |format|
+            format.html
+            format.json do
+              error_response(generate_error_info(service))
+            end
+          end
+        end
+      end
     end
 
     private
-      # Use callbacks to share common setup or constraints between actions.
-      def set_attachment
-        @attachment = Attachment.find(params[:id])
-      end
-
       # Only allow a trusted parameter "white list" through.
       def attachment_params
         params.require(:attachment).permit(:attachable, :author, :title, :caption)
