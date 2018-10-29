93c5d2c25e34e7066e7f5ba3e9e808555c798407
Emil Scoffone <emil@scoffone.com>
2018-10-28 21:57:43 -0700

    bumped the version number after merge of service-generator

--- lib/fl/framework/version.rb
+++ lib/fl/framework/version.rb
@@ -2,10 +2,10 @@ module Fl
   module Framework
     # The version number of the fl-framework gem.
 
-    VERSION = '0.7.0'
+    VERSION = '0.7.1'
 
     # The version date for the fl-framework gem.
 
-    DATE = '2018-10-16'
+    DATE = '2018-10-28'
   end
 end
