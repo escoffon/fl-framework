038fd55f1c121fbac4e8c6040963c4189f2b9eb1
Emil Scoffone <emil@scoffone.com>
2018-12-03 22:02:08 -0800

    Added support for clients that pack query string parameters as JSON

--- lib/fl/framework/version.rb
+++ lib/fl/framework/version.rb
@@ -2,10 +2,10 @@ module Fl
   module Framework
     # The version number of the fl-framework gem.
 
-    VERSION = '0.7.10'
+    VERSION = '0.7.11'
 
     # The version date for the fl-framework gem.
 
-    DATE = '2018-11-28'
+    DATE = '2018-12-03'
   end
 end
