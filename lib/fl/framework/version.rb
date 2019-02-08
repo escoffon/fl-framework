44096952a035fb5f1778d7117890953bfae6d1d0
Emil Scoffone <emil@scoffone.com>
2019-02-08 04:29:03 -0800

    Bumped version number to 0.9 after list objects merge

--- lib/fl/framework/version.rb
+++ lib/fl/framework/version.rb
@@ -2,10 +2,10 @@ module Fl
   module Framework
     # The version number of the fl-framework gem.
 
-    VERSION = '0.8.5'
+    VERSION = '0.9.0'
 
     # The version date for the fl-framework gem.
 
-    DATE = '2019-01-18'
+    DATE = '2019-02-08'
   end
 end
